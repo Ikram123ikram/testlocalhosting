ac7ef43bb5182fedcd5022300c29826566805cef
Ikram123ikram <88238942+Ikram123ikram@users.noreply.github.com>
2022-07-19 14:14:44 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+$host = '127.0.0.1';
+$user = 'root';
+$pass = '';
+$dbName = 'test_hosting';
+$con = mysqli_connect($host,$user,$pass);
+$link=mysqli_select_db($con,$dbName);
+
+?>
+<?php 
+if(isset($_POST['submit'])){
+    $nom=$_POST['nom'];
+    $prenom=$_POST['prenom'];
+    
+    $query = "INSERT INTO testhosting(nom,prenom) VALUES('$nom','$prenom')";
+    $result = mysqli_query($con, $query);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test local hosting</title>
+</head>
+<body>
+   <form action="index.php" method="POST">
+   <label>Nom</label>
+   <input type="text" name="nom">
+   <br><br>
+   <label>Prenom</label>
+   <input type="text" name="prenom">
+   <br><br>
+   <input type="submit" name="submit" >
+   </form> 
+
+</body>
+</html>
